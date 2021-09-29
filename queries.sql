d1b47c9f6cc4af96266735dc8edd2a72d95d6e04
Michal Pawel Wadowski <80638034+Michau94@users.noreply.github.com>
2021-09-29 15:04:32 +0200

    JOIN query 3

--- queries.sql
+++ queries.sql
@@ -89,3 +89,21 @@ JOIN `degrees`
 ON `degrees`.`id` = `students`.`degree_id` 
 WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
 
+-- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+
+SELECT `degrees`.`name`, `departments`.`name` 
+FROM `degrees` 
+JOIN `departments` 
+ON `departments`.`id` = `degrees`.`department_id` 
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
+
+-- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+
+SELECT `courses`.`name` AS `nome_corso` , `teachers`.`name`, `teachers`.`surname`
+FROM `course_teacher`
+JOIN `teachers`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
+JOIN `courses`
+ON `courses`.`id` = `course_teacher`.`course_id`
+WHERE `teachers`.`name` = 'Fulvio'
+AND `teachers`.`surname` = 'Amato';
