e12f0da909948223d03846ddb99ba92780f0cea1
Michal Pawel Wadowski <80638034+Michau94@users.noreply.github.com>
2021-09-29 14:48:04 +0200

    JOIN query 1

--- queries.sql
+++ queries.sql
@@ -76,3 +76,16 @@ GROUP BY `exam_id` ;
 SELECT `department_id`, COUNT(*) AS `courses` 
 FROM `degrees` 
 GROUP BY `department_id` 
+
+
+
+ -- JOIN QUERIES
+
+-- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+
+SELECT `students`.`name`, `students`.`surname`, `degrees`.`name` 
+FROM `students` 
+JOIN `degrees` 
+ON `degrees`.`id` = `students`.`degree_id` 
+WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
+
